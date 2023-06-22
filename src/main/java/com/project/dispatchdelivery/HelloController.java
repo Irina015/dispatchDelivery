ebf452213c2f155b4cb76d44406a3931cb182a2a
vivilookup <zhouvivi316@gmail.com>
2023-06-21 18:33:22 -0700

    add OrderController

--- src/main/java/com/project/dispatchdelivery/HelloController.java
+++ src/main/java/com/project/dispatchdelivery/HelloController.java
@@ -7,17 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    private final OrderRepository orderRepository;
-
-    public HelloController(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
 
     @GetMapping("/hello")
     public String sayHello() {
 
-        OrderEntity newOrder = new OrderEntity(null, 1L,"beijing","new york", "July20","July25");
-        orderRepository.save(newOrder);
         System.out.println("springboot started");
         return "hello world";
     }
--- src/main/java/com/project/dispatchdelivery/order/OrderController.java
+++ src/main/java/com/project/dispatchdelivery/order/OrderController.java
@@ -1,4 +1,20 @@
 package com.project.dispatchdelivery.order;
 
+import com.project.dispatchdelivery.db.OrderRepository;
+import com.project.dispatchdelivery.db.entity.OrderEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+    private final OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+    @GetMapping("/createOrder")
+    public void createOrder(){
+        OrderEntity newOrder = new OrderEntity(null, 1L,"beijing","new york", "July20","July25");
+        orderRepository.save(newOrder);
+    }
 }
