06266312da81d030b3b694634002452e95657fe6
irina015 <irinaliang015@gmail.com>
2023-06-24 23:27:59 -0700

    new update

--- src/main/java/com/project/dispatchdelivery/db/entity/UsersEntity.java
+++ src/main/java/com/project/dispatchdelivery/db/entity/UsersEntity.java
@@ -9,13 +9,9 @@ public record UsersEntity(
         @Id long id,
         String username,
         String password,
-        @JsonProperty("first_name")
         String firstName,
-        @JsonProperty("last_name")
         String lastName,
-        @JsonProperty("phone_number")
         String phoneNumber,
-        @JsonProperty("email_address")
         String emailAddress
 ){
 }
--- src/main/java/com/project/dispatchdelivery/model/RegisterBody.java
+++ src/main/java/com/project/dispatchdelivery/model/RegisterBody.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record RegisterBody(
         String username,
         String password,
-        @JsonProperty("first_name") String firstName,
-        @JsonProperty("last_name") String lastName,
-        @JsonProperty("phone_number") String phoneNumber,
-        @JsonProperty("email_address") String emailAddress
+        String firstName,
+        String lastName,
+        String phoneNumber,
+        String emailAddress
 ) {
 }
